c60162aca441b6ebdbf714b9666622c7938cfe86
meghnavenkatesha <venkatesha.m@husky.neu.edu>
2017-12-10 14:22:58 -0500

    Update TFIDF.py
    Updating changes

--- TFIDF.py
+++ TFIDF.py
@@ -1,14 +1,21 @@
 from bs4 import BeautifulSoup
+from nltk import ngrams
 import math
-import corpus
-import indexer
 import os
+import re
+import traceback
+import glob
 
 dlist = []
 tfidf_count = {}
 dlength = {}
 ind_list = {}
 qr_list = {}
+file_list = []
+dir = 'cacm'
+unigram = []
+indexer = {}
+tf = {}
 
 
 def call_jobs():
@@ -30,7 +37,7 @@ def call_jobs():
             tfidf_count[each_document] = 0.0
         tfidf_count = tfidf(ind_list, qr_list[query_id], x, dlength, tfidf_count, [])
         tfidf_score = sorted(tfidf_count.items(), key=lambda a : a[1], reverse=True)
-        write_file(query_id, tfidf_score[:100], "TFIDF_Output", "tfidf")
+        write_file_tfidf(query_id, tfidf_score[:100], "TFIDF_Output", "tfidf")
 
 
 def tfidf(index, query, n, doc_length, score, stopword):
@@ -47,7 +54,7 @@ def tfidf(index, query, n, doc_length, score, stopword):
     return score
 
 
-def write_file(query_id, val, name, alg_name):
+def write_file_tfidf(query_id, val, name, alg_name):
     if not os.path.exists("Phase1"):
         os.makedirs("Phase1")
     ranking = 1
@@ -76,7 +83,7 @@ def read_query():
         qr = text.get_text().strip(' \n\t')
         qr = str(qr)
         qr = qr.lower()
-        qr = corpus.transformation(qr)
+        qr = transformation(qr)
         write_query_file(counter, qr)
         query[counter] = qr.split(" ")
         counter += 1
@@ -92,12 +99,118 @@ def write_query_file(query_id, queries):
             f.write(str(query_id) + " " + queries + "\n")
 
 
+def transformation(data):
+    data = re.sub(r'[@_!\s^&*?#=+$~%:;\\/|<>(){}[\]"\']', ' ', data)
+    term_list = []
+    for term in data.split():
+        term_len = len(term)
+        if term[term_len - 1:term_len] == '-' \
+                or term[term_len - 1:term_len] == ',' \
+                or term[term_len - 1:term_len] == '.':
+            term = term[:term_len - 1]
+            term_list.append(handle_punctuation(term))
+        else:
+            term_list.append(handle_punctuation(term))
+    term_list = [x for x in term_list if x != '']
+    term_list = " ".join(term_list)
+    if ' PM ' in term_list or 'PM ' in term_list or 'PMB ' in term_list:
+        term_list_proc = term_list.split('PM')[0]
+        term_list_proc += " pm"
+        return term_list_proc
+    elif ' AM ' in term_list or 'AM ' in term_list:
+        term_list_proc = term_list.split('AM')[0]
+        term_list_proc += " am"
+        return term_list_proc
+    else:
+        return term_list
+
+
+def handle_punctuation(term):
+    while term[:1] == "-" or term[:1] == "," or term[:1] == ".":
+        if re.match(r'^[\-]?[0-9]*\.?[0-9]+$', term):
+            return term
+        if term[:1] == "-" or term[:1] == "." or term[:1] == ",":
+            term = term[1:]
+        else:
+            return term
+    return term
+
+
+def fetch_data():
+    try:
+        counter = 1
+        for filename in glob.glob(os.path.join('*.html')):
+            with open(filename) as f:
+                article = filename.strip('cacm/').strip('.html')
+                data = f.read()
+                counter += 1
+                soup = BeautifulSoup(data, 'html.parser')
+                soup.prettify().encode('utf-8')
+                text = soup.find('text').get_text().encode('utf-8')
+                content = text
+                processed_data = transformation(content)
+                processed_data = processed_data.lower()
+                write_file_corpus(processed_data, article)
+                f.close()
+    except:
+        print("Try block error")
+        print(traceback.format_exc())
+
+
+def write_file_corpus(data, file_name):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        index_terms = open(dir + '/' + file_name + '.txt', 'w')
+        index_terms.write(data)
+        index_terms.close()
+    except:
+        print("Try block error")
+        print(traceback.format_exc())
+
+
+def generate_index(files, gram):
+    temp = gram
+    for item in gram:
+        if item in indexer:
+            for one in indexer[item]:
+                equal = 1
+                if files == one[0]:
+                    equal *= 0
+                else:
+                    equal *= 1
+            if equal == 1:
+                indexer[item] += [[files,counter(item,temp)]]
+        else:
+            indexer[item] = [[files,counter(item,temp)]]
+
+
+def counter(term, grams):
+    total = 0
+    for items in grams:
+        if term == items:
+            total += 1
+    return total
+
+
+def index_creation(dir):
+    global unigram, indexer
+    indexer = {}
+    for file in os.listdir(dir):
+        if ".DS_Store" not in file:
+            for item in ngrams((open(dir + file, 'r').read()).split(),1):
+                unigram.append(item[0])
+            generate_index(file, unigram)
+            unigram = []
+    return indexer
+
+
 def main():
     global ind_list, dlist
     print("Corpus Generation")
-    corpus.fetch_data()
+    fetch_data()
     print("Indexer")
-    ind_list = indexer.index_creation("cacm/")
+    ind_list = index_creation("cacm/")
     for one in ind_list:
         for item in ind_list[one]:
             if item[0] not in dlist:
